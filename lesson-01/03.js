f6d7da754237868556ac1224b2a283439bce7cc4
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-02-07 11:35:10 +0300

    lesson2

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -2,4 +2,9 @@ let number = 1;
 console.log(number++);
 console.log(++number);
 console.log(number--);
-console.log(--number); 
+console.log(--number);
+
+
+var guess = +prompt("number: ")
+var guess1 = +prompt("number1: ")
+console.log(guess + guess1)
