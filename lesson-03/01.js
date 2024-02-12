4b7a50796eda23d34f34fb3efef16a562cffb9fe
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-02-12 20:56:20 +0300

    l3

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    let col = colors.filter(function (c) {
+      if (c == "черный" || c == "красный" || c == "желтый")
+        return true
+    })
+  return col.join("-")
+}
+
+console.log(createColorString());
