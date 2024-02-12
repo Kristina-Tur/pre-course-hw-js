4de18de616be95f4e096b39b1ae3df8ee7574905
Kristina-Tur <kristina.tur06121955@gmail.com>
2024-02-12 21:29:39 +0300

    l3

---
+++ lesson-03/03.js
@@ -0,0 +1,3 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+const fruitsSub = fruits.slice(1, length - 2)
+console.log(fruitsSub)
